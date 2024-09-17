d053bc88729f699926f8e7be9e62a91ba084ff64
FernandoB <fernando.boluda@sanoma.com>
2024-09-17 09:51:52 +0200

    Implement HttpRequest

--- src/http/request.rs
+++ src/http/request.rs
@@ -1,12 +1,44 @@
-use std::{collections::HashMap, fmt::Display, str::FromStr};
+use std::{collections::HashMap, fmt::Display, io, str::FromStr};
 
 #[derive(Debug)]
 pub struct HttpRequest {
     method: Method,
-    route: Resource,
+    resource: Resource,
     version: Version,
     headers: HttpHeader,
-    request_Body: String,
+    pub request_body: String,
+}
+
+impl HttpRequest {
+    pub fn new(request: &str) -> io::Result<HttpRequest> {
+        let method: Method = Method::new(request);
+        let resource: Resource = if let Some(resource) = Resource::new(request) {
+            resource
+        } else {
+            Resource { path : "".to_string() }
+        };
+        let version: Version = Version::new(request).map_err( |err | {
+            io::Error::new(io::ErrorKind::InvalidData, err.msg)
+        })?;
+        let headers: HttpHeader =  if let Some(headers) = HttpHeader::new(request) {
+            headers
+        } else {
+            HttpHeader { headers : HashMap::new() }
+        };
+        let request_body: String =  if let Some((_header, body)) = request.split_once("\r\n\r\n") {
+            body.to_string()
+        } else {
+            String::new()
+        };
+
+        Ok(HttpRequest {
+            method,
+            resource,
+            version,
+            headers,
+            request_body,
+        })
+    }
 }
 
 #[derive(Debug)]
