02f5adbb5f5f271674279f8e0c91a5f8ed2fb786
FernandoB <fernando.boluda@sanoma.com>
2024-09-17 11:34:04 +0200

    Create response

--- src/http/request.rs
+++ src/http/request.rs
@@ -3,7 +3,7 @@ use std::{collections::HashMap, fmt::Display, io, str::FromStr};
 #[derive(Debug)]
 pub struct HttpRequest {
     method: Method,
-    resource: Resource,
+    pub resource: Resource,
     version: Version,
     headers: HttpHeader,
     pub request_body: String,
@@ -77,13 +77,23 @@ impl Method {
 }
 
 #[derive(Debug)]
-enum Version {
+pub enum Version {
     V1_1,
     V2_0,
 }
 
+impl Display for Version {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let msg = match self {
+            Version::V1_1 => "HTTP/1.1",
+            Version::V2_0 => "HTTP/2",
+        };
+        write!(f, "{}", msg)
+    }
+}
+
 #[derive(Debug)]
-struct VersionError {
+pub struct VersionError {
     msg: String,
 }
 
@@ -121,8 +131,8 @@ impl FromStr for Version {
 }
 
 #[derive(Debug)]
-struct Resource {
-    path: String,
+pub struct Resource {
+    pub path: String,
 }
 
 impl Resource {
